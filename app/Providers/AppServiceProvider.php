c7858fe7f3aee8ce04b85044aff8c414cfd68ec9
pimenvibritania <blackbrit13@gmail.com>
2018-02-16 14:48:10 +0700

    service https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,13 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
+    if ($this->app->environment() == 'production') {
+        URL::forceScheme('https');
+    }
     /**
      * Bootstrap any application services.
      *
